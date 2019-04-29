6866a9aadc8b86847a8c9ed591d74ca805098ddb
Arthur <arthur_sbd@hotmail.com>
2019-04-29 15:20:47 -0300

    ranking

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 hbs.registerPartials(__dirname + '/views/partials')
+console.log('###########', __dirname)
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
