39f0c585dae5de5685b2925453f972c609f729cd
Arnold Lafuente <thanos_alu001@hotmail.com>
2019-04-30 18:40:42 -0300

    routes linked to each view

--- routes/event.js
+++ routes/event.js
@@ -1,9 +1,34 @@
 const express = require('express');
-const router  = express.Router();
+
+const router = express.Router();
+const Events = require('../models/Event.js');
 
 /* GET event page */
-router.get('/event/:id', (req, res, next) => {
-  res.render('event-detail');
+
+router.get('/', (req, res, next) => {
+  Events.find()
+    .then((result) => {
+      let mainEvents;
+      let leftEvents;
+      const a = result.sort((a, b) => b.rate - a.rate);
+      mainEvents = a.slice(0, 3);
+      leftEvents = a.slice(3);
+      res.render('home', { mainEvent: mainEvents, leftEvent: leftEvents });
+    })
+    .catch((error) => {
+      console.log('Error while retrieving events details: ', error);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  Events.findById({ _id: req.params.id })
+    .then((result) => {
+      console.log(result);
+      res.render('event-detail', { event: result });
+    })
+    .catch((error) => {
+      console.log('Error while retrieving event details: ', error);
+    });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const Users = require('../models/User.js');
-const Events = require('../models/Event.js');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -20,27 +20,12 @@ router.get('/sign-in', (req, res, next) => {
   res.render('sign-in');
 });
 
-router.get('/home', (req, res, next) => {
-  Events.find()
-    .then((result) => {
-      let mainEvents;
-      let leftEvents;
-      const a = result.sort((a, b) => b.rate - a.rate);
-      mainEvents = a.slice(0, 3);
-      leftEvents = a.slice(3);
-      res.render('home', { mainEvent: mainEvents, leftEvent: leftEvents });
-    })
-    .catch((error) => {
-      console.log('Error while retrieving events details: ', error);
-    });
-});
+
 
 router.get('/ranking', (req, res, next) => {
   res.render('ranking');
 });
 
-router.get('/event/:id', (req, res, next) => {
-  res.render('event-detail');
-});
+
 
 module.exports = router;
