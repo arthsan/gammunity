46a793631cbaa85921878fbc2c170be24da90e14
Arnold Lafuente <thanos_alu001@hotmail.com>
2019-05-02 09:52:58 -0300

    CRUD working with some errors

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -106,8 +106,7 @@ router.post('/events/new', ensureLogin.ensureLoggedIn(), uploadCloud.single('pho
     title, category, clan, text, date,
   } = req.body;
   const photo = req.file.url;
-  const photoName = req.file.originalname;
-  console.log(photo);
+  const photoName = req.file.originalname;  
   const newEvent = new Events({
     title, category, photoName, photo, clan, text, date,
   });
@@ -119,17 +118,6 @@ router.post('/events/new', ensureLogin.ensureLoggedIn(), uploadCloud.single('pho
       console.log(error);
     });
   res.render('createEvent', { user: req.user });
-});
-
-
-router.get('/events/:id', ensureLogin.ensureLoggedIn(), (req, res, next) => {
-  Events.findById({ _id: req.params.id })
-    .then((result) => {
-      res.render('event-detail', { user: req.user, event: result });
-    })
-    .catch((error) => {
-      console.log('Error while retrieving event details: ', error);
-    });
 });
 
 router.get('/events/:id/edit', ensureLogin.ensureLoggedIn(), (req, res, next) => {
@@ -161,6 +149,16 @@ router.get('/events/:id/delet', ensureLogin.ensureLoggedIn(), (req, res, next) =
   Events.deleteOne({ _id: req.params.id })
     .then((result) => {
       res.redirect('home');
+    })
+    .catch((error) => {
+      console.log('Error while retrieving event details: ', error);
+    });
+});
+
+router.get('/events/:id', ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  Events.findById({ _id: req.params.id })
+    .then((result) => {
+      res.render('event-detail', { user: req.user, event: result });
     })
     .catch((error) => {
       console.log('Error while retrieving event details: ', error);
