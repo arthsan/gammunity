34c94263003b5c9b1821b0345b5612ca14738b2f
Arthur <arthur_sbd@hotmail.com>
2019-05-03 11:37:47 -0300

    arthsan

--- routes/articles.js
+++ routes/articles.js
@@ -34,9 +34,10 @@ router.post('/new', ensureLogin.ensureLoggedIn(), uploadCloud.single('photo'), (
 router.get('/:id', (req, res, next) => {
   Articles.findById({ _id: req.params.id })
     .then((result) => {
+      res.render('article', { article: result });
       if (req.user.role === 'ADMIN') {
         res.render('article', { admin: req.user.role, user: req.user, article: result });
-      } else {
+      } if (req.user.role === 'GUEST') {
         res.render('article', { user: req.user, article: result });
       }
     });
@@ -76,7 +77,5 @@ router.get('/:id/delet', (req, res, next) => {
       console.log('Error while retrieving event details: ', error);
     });
 });
-
-
 
 module.exports = router;
